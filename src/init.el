ead48fa09a70069d0d05f398d01368fab85fd561
Jonathan Pulsifer <jonathan@pulsifer.ca>
2021-02-10 21:51:17 -0500

    moar emacs
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- src/init.el
+++ src/init.el
@@ -1,2 +1,6 @@
 (set-frame-font "Fira Code 14" nil t)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+(xterm-mouse-mode 1)
+(custom-set-variables
+ '(custom-enabled-themes '(tango-dark)))
+(custom-set-faces)
